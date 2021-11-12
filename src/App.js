fffde6f4f84db08de75408e280213a6139db4f2f
Charlie-007-star <sujinsr2457@gmail.com>
2021-11-12 12:54:24 +0530

    todo list going on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
+import { useState } from "react"
 import Header from "./components/Header"; 
+import Tasks from "./components/Tasks";
+
+
 
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Task 1",
+        day:'Feb 5 at 2:20pm',
+        reminder:true
+    },
+    {
+        id: 2,
+        text: "Task 2",
+        day:'Feb 6 at 2:20pm',
+        reminder:false
+    },
+    {
+        id: 3,
+        text: "Task 3",
+        day:'Feb 7 at 2:20pm',
+        reminder:true
+    },
+])
+
+/* ------------------------------- Delete task ------------------------------ */
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
+
+/* ----------------------------- Toggle reminder ---------------------------- */
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task)=>task.id === id ? {...task, reminder:!task.reminder}:task))
+}
+
 
   return (
     <div className="App">
       <Header />
+      {tasks.length > 0? (
+        <Tasks tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder}
+        />):(
+          'No Task to show'
+          )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
+import Button from './Button'
 
-export const Header = ({title}) => {
+const onClick = (e) => {
+        console.log('clicked')
+    }
+
+ const Header = ({title}) => {
     return (
-        <header>
+        <header className="header">
             <h1>{title}</h1>
+            <Button color="Black" text="Add" onClick={onClick}/>
         </header>
     )
 } 
